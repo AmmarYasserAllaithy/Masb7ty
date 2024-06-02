987120d336f82a16201e4b5e661828eaf7437a0c
Ammar Yasser Allaithy <ammaryasserallaithy@gmail.com>
2024-06-02 17:53:01 +0300

    add util

---
+++ app/src/main/java/com/ammaryasser/masb7ty/util/util.kt
@@ -0,0 +1,90 @@
+package com.ammaryasser.masb7ty.util
+
+import android.app.Activity
+
+import android.content.Context
+import android.content.ContextWrapper
+
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+
+import java.util.Locale
+
+
+val rocoshape = RoundedCornerShape(16.dp)
+
+
+sealed class Screen(val route: String) {
+
+    object Tasabee7 : Screen("tasabee7_screen")
+
+    object Masba7ty : Screen("masba7ty_screen") {
+        val ID_KEY = "id"
+        fun fullRoute() = "$route?{$ID_KEY}"
+        fun routeOfTasbee7Id(id: Int) = "$route?$id"
+    }
+
+    object About : Screen("about_screen")
+
+}
+
+
+fun vibrateOneShot(ctx: Context, millis: Long) {
+    val vibrator = ctx.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        vibrator.run {
+            cancel()
+            vibrate(VibrationEffect.createOneShot(millis, VibrationEffect.DEFAULT_AMPLITUDE))
+        }
+}
+
+
+@Composable
+fun ArabLocale(isAr: Boolean = true) {
+    val locale = Locale(if (isAr) "ar" else "en")
+    val configuration = LocalConfiguration.current
+    val resources = LocalContext.current.resources
+
+    Locale.setDefault(locale)
+    configuration.setLocale(locale)
+
+    //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+    //	createConfigurationContext(configuration)
+
+    resources.updateConfiguration(configuration, resources.displayMetrics)
+}
+
+
+@Composable
+fun LockScreenOrientation(orientation: Int) {
+    val context = LocalContext.current
+
+    DisposableEffect(Unit) {
+        val activity = context.findActivity() ?: return@DisposableEffect onDispose {}
+        val originalOrientation = activity.requestedOrientation
+        activity.requestedOrientation = orientation
+
+        onDispose {
+            // restore original orientation when view disappears
+            activity.requestedOrientation = originalOrientation
+        }
+    }
+}
+
+
+fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
+}
